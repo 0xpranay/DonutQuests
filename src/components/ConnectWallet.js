a2a1449dca5f623b1ec6a752a62d6143715b5a49
Pranay Reddy <pranayreddy1024@gmail.com>
2022-02-07 13:55:02 +0530

    Wallet integrations

--- src/components/ConnectWallet.js
+++ src/components/ConnectWallet.js
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 import styles from "./styles/connectWallet.module.scss";
+import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
+import { FortmaticConnector } from "@web3-react/fortmatic-connector";
+import { PortisConnector } from "@web3-react/portis-connector";
 import {
   InjectedConnector,
   NoEthereumProviderError,
 } from "@web3-react/injected-connector";
 import Jazzicon, { jsNumberForAddress } from "react-jazzicon";
-import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
+
 import { useWeb3React } from "@web3-react/core";
 import ReactModal from "react-modal";
-
+// require("dotenv").config();
+console.log(process.env.REACT_APP_FORTMATIC);
+console.log(process.env.REACT_APP_PORTIS);
 function WalletModal(props) {
   const { account, active, activate, deactivate } = useWeb3React();
   ReactModal.setAppElement("#root");
@@ -38,7 +43,16 @@ function WalletModal(props) {
           </button>
         </div>
         <div className={styles.wallets}>
-          <button className={styles.wallet}>
+          <button
+            className={styles.wallet}
+            onClick={async () => {
+              await activate(
+                new InjectedConnector({
+                  supportedChainIds: [100],
+                })
+              );
+            }}
+          >
             <div>{window.ethereum ? "Metamask" : "Install Metamask"}</div>
             <div>
               <img
@@ -49,7 +63,23 @@ function WalletModal(props) {
               />
             </div>
           </button>
-          <button className={styles.wallet}>
+          <button
+            className={styles.wallet}
+            onClick={async () => {
+              console.log("WC");
+              await activate(
+                new WalletConnectConnector({
+                  supportedChainIds: [100],
+                  rpc: {
+                    100: "https://dai.poa.network/",
+                  },
+                  bridge: "https://bridge.walletconnect.org",
+                  qrcode: true,
+                })
+              );
+              console.log("WC-1");
+            }}
+          >
             <div>WalletConnect</div>
             <div>
               <img
@@ -71,7 +101,17 @@ function WalletModal(props) {
               />
             </div>
           </button>
-          <button className={styles.wallet}>
+          <button
+            className={styles.wallet}
+            onClick={async () => {
+              await activate(
+                new FortmaticConnector({
+                  apiKey: process.env.REACT_APP_FORTMATIC,
+                  chainId: 1,
+                })
+              );
+            }}
+          >
             <div>Fortmatic</div>
             <div>
               <img
@@ -82,7 +122,17 @@ function WalletModal(props) {
               />
             </div>
           </button>
-          <button className={styles.wallet}>
+          <button
+            className={styles.wallet}
+            onClick={async () => {
+              await activate(
+                new PortisConnector({
+                  dAppId: process.env.REACT_APP_PORTIS,
+                  networks: [100],
+                })
+              );
+            }}
+          >
             <div>Portis</div>
             <div>
               <img
@@ -99,19 +149,52 @@ function WalletModal(props) {
   );
 }
 export default function ConnectWallet() {
-  const { account, active, activate, deactivate } = useWeb3React();
+  const { account, active, activate, deactivate, library } = useWeb3React();
   const [modal, setModal] = useState(false);
   function toggleModal() {
     if (modal) document.body.style.overflow = "visible";
     if (!modal) document.body.style.overflow = "hidden";
     setModal((modal) => !modal);
   }
+  async function sign() {
+    let input = prompt("Enter Message");
+    const message = await library.getSigner(account).signMessage(input);
+    alert(message);
+  }
+
+  async function send() {
+    await library.getSigner(account).sendTransaction({
+      nonce: account.nonce, // 0 in decimal
+      gasLimit: "0x5208", // 21000 in decimal
+      gasPrice: null, // 1000000000 in decimal
+      to: "0x17A98d2b11Dfb784e63337d2170e21cf5DD04631",
+      value: "0x9184E72A000", // 100000000000000000 in decimal
+      data: "0x", // “empty” value in decimal
+      // "chainId": 4 // Ethereum network id
+    });
+  }
   return (
     <>
-      <button onClick={toggleModal} className={styles.connect}>
-        Connect Wallet
-      </button>
-      <WalletModal show={modal} toggleModal={toggleModal}></WalletModal>
+      {!active ? (
+        <div>
+          <button onClick={toggleModal} className={styles.connect}>
+            Connect Wallet
+          </button>
+          <WalletModal show={modal} toggleModal={toggleModal}></WalletModal>
+        </div>
+      ) : (
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <button className={styles.connect} onClick={deactivate}>
+            Connected
+          </button>
+          <button className={styles.connect} onClick={sign}>
+            Sign
+          </button>
+          <button className={styles.connect} onClick={send}>
+            Send
+          </button>
+        </div>
+      )}
     </>
   );
 }
