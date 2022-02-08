812f557044f85d7c57bc82301e9778e890b5ad8d
Pranay Reddy <pranayreddy1024@gmail.com>
2022-02-08 21:06:51 +0530

    Added graph objects

--- src/components/QuestPlate.js
+++ src/components/QuestPlate.js
@@ -1,17 +1,61 @@
 import React from "react";
 import Quest from "./Quest";
 import styles from "./styles/questPlate.module.scss";
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
+import tasks from "../queries/tasks";
+
 export default function QuestPlate() {
   return (
     <div className={styles.questPlate}>
-      <Quest></Quest>
-      <Quest></Quest>
-      <Quest></Quest>
-      <Quest></Quest>
-      <Quest></Quest>
-      <Quest></Quest>
-      <Quest></Quest>
-      <Quest></Quest>
+      {/* <ApolloProvider>
+        <Quest></Quest>
+      </ApolloProvider>
+      <ApolloProvider>
+        <Quest></Quest>
+      </ApolloProvider>
+      <ApolloProvider>
+        <Quest></Quest>
+      </ApolloProvider>
+      <ApolloProvider>
+        <Quest></Quest>
+      </ApolloProvider>
+      <ApolloProvider>
+        <Quest></Quest>
+      </ApolloProvider>
+      <ApolloProvider>
+        <Quest></Quest>
+      </ApolloProvider>
+      <ApolloProvider>
+        <Quest></Quest>
+      </ApolloProvider>
+      <ApolloProvider>
+        <Quest></Quest>
+      </ApolloProvider> */}
+      {tasks.map(function (task) {
+        const httpLink = createHttpLink({
+          uri: task.uri,
+        });
+        const client = new ApolloClient({
+          link: httpLink,
+          cache: new InMemoryCache(),
+        });
+        return (
+          <ApolloProvider client={client}>
+            <Quest
+              key={task.id}
+              title={task.title}
+              id={task.id}
+              wait={task.wait}
+              claim={task.claim}
+            ></Quest>
+          </ApolloProvider>
+        );
+      })}
     </div>
   );
 }
---
+++ src/queries/tasks.js
@@ -0,0 +1,66 @@
+import { gql } from "@apollo/client";
+const tasks = [
+  {
+    id: 0,
+    title: "Some task title",
+    uri: "HTTP URI",
+    wait: gql`
+      {
+        feed {
+          id
+          links {
+            id
+            createdAt
+            url
+            description
+          }
+        }
+      }
+    `,
+    claim: gql`
+      {
+        feed {
+          id
+          links {
+            id
+            createdAt
+            url
+            description
+          }
+        }
+      }
+    `,
+  },
+  {
+    id: 0,
+    title: "Some task title",
+    uri: "HTTP URI",
+    wait: gql`
+      {
+        feed {
+          id
+          links {
+            id
+            createdAt
+            url
+            description
+          }
+        }
+      }
+    `,
+    claim: gql`
+      {
+        feed {
+          id
+          links {
+            id
+            createdAt
+            url
+            description
+          }
+        }
+      }
+    `,
+  },
+];
+export default tasks;
